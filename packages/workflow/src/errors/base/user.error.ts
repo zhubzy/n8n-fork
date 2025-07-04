60b94c586d209e7a977b3a4dd1aca70602cce9de
Zach Zhong <zzyzsy0516321@yahoo.com>
2025-07-04 11:51:53 -0700

    fix type check

--- packages/workflow/src/errors/base/user.error.ts
+++ packages/workflow/src/errors/base/user.error.ts
@@ -8,14 +8,12 @@ export type UserErrorOptions = Omit<BaseErrorOptions, 'level'> & {
 
 /**
  * Error that indicates the user performed an action that caused an error.
- * E.g. provided invalid input, tried to access a resource they’re not
+ * E.g. provided invalid input, tried to access a resource they're not
  * authorized to, or violates a business rule.
  *
  * Default level: info
  */
 export class UserError extends BaseError {
-	readonly description: string | null | undefined;
-
 	constructor(message: string, opts: UserErrorOptions = {}) {
 		opts.level = opts.level ?? 'info';
 
